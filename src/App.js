f8c2c2f8b25091980d45d7ec8fc3c785e27bc1a7
sidee <said.issack@student.moringaschool.com>
2023-07-23 09:41:22 +0300

    added some css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Trans from './components/transaction';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className='main'>
+      <Trans/>
+    </div>
+    
+  );
+}
+
+export default App;
